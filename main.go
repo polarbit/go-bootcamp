e12c61c042dd76d035100f82227ee2ff4b0c0a96
Şafak Ulusoy <safakulusoy@gmail.com>
2021-08-29 10:39:40 +0000

    Flags are introduced so that samples are only run if related flags are provided.

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path"
+	"unicode/utf8"
 )
 
+var argFile string
+var argUser string
+
 func main() {
+	fmt.Println("Hello Me! Let's continue learning...")
+
+	flag.StringVar(&argFile, "file", "", "If given, filename part is printed.")
+	flag.StringVar(&argUser, "user", "", "If given, string length is printed.")
+	flag.Parse()
+
 	args := os.Args
-	fmt.Println("Hello Me!")
-	fmt.Println(getFilename(args[1]))
+	fmt.Printf("Args: %#v \n", args)
+
+	if argFile != "" {
+		printFilename(argFile)
+	}
+
+	if argUser != "" {
+		printStrLen(argUser)
+	}
 }
 
-func getFilename(filename string) string {
+func printFilename(filename string) {
 	_, path := path.Split(filename)
-	return path
+	fmt.Println("Filename:", path, "full-path:", argFile)
+}
+
+func printStrLen(s string) {
+	fmt.Printf("%v bytes=%v length=%v\n", s, len(s), utf8.RuneCountInString(s))
 }
